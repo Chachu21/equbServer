7d7c6f7c9b02d417733ab65a91e773bf8452673a
Chachu21 <tsecha6@gmail.com>
2024-08-13 21:56:54 +0300

    final code

--- index.js
+++ index.js
@@ -1,24 +1,49 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import bodyParser from "body-parser";
 import createDatabase from "./config/dbConfig.js";
+import userRouter from "./routes/userRoute.js";
+import groupRouter from "./routes/groupRoutes.js";
+import commentRouter from "./routes/commentRoutes.js";
+import paymentRouter from "./routes/paymentRoute.js";
+import requestRouter from "./routes/requestRoutes.js";
+import userScheduleAnnouncement from "./utils/userAnnouncement.js";
+// import adminUpaideAnnouncement from "./utils/announcementAndcheckUpaidMember.js";
+import winnerSelection from "./utils/automticSelectWinner.js";
+import notificationRouter from "./routes/notificationRoute.js";
 
-dotenv.config();
+//instantait the express
 const app = express();
-const port = process.env.PORT || 5000;
-
-// Connect to the database
+dotenv.config();
+const port = process.env.PORT;
+//connect to the database
 createDatabase();
-
 // Middleware
 app.use(cors());
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+// Increase the limit for request payload size
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
-// Routes
+// Define your routes here
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/group", groupRouter);
+app.use("/api/v1/comment", commentRouter);
+app.use("/api/v1/payment", paymentRouter);
+app.use("/api/v1/request", requestRouter);
+app.use("/api/v1/notification", notificationRouter);
+
+//for user announcements
+userScheduleAnnouncement();
+//for admin  about unpaid announcements
+// adminUpaideAnnouncement();
+//for winner
+winnerSelection();
 
 // Start the server
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
