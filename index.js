24091596c54cc26f51c827f6e72092fffad8882c
Chachu21 <tsecha6@gmail.com>
2024-08-13 21:47:52 +0300

    update the code by adding some code

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 import express from "express";
-import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import createDatabase from "./config/dbConfig.js";
+
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
-//connect to the database
+
+// Connect to the database
 createDatabase();
+
 // Middleware
 app.use(cors());
-// Increase the limit for request payload size
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
-app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+// Start the server
 app.listen(port, () => console.log(`Server running on port ${port}`));
